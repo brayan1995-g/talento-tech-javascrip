d707838e32c2b3735fe4ff95e2aef11361a16974
brayan1995-g <yyontec@gmail.com>
2025-08-12 19:42:07 -0500

    trabajo en clase con javascrip

---
+++ function-fecha.js
@@ -0,0 +1,19 @@
+function numeroDelafuncion(n1, n2) {
+
+    console.log("los numeros son:", n1, n2)
+}
+
+const variable() => {
+
+
+}
+
+const imprimirNombre (nombres;) => {
+
+    return "hola" nombres;
+
+
+}
+    resultado imprimir("edwin");
+console.log(resultado);
+
